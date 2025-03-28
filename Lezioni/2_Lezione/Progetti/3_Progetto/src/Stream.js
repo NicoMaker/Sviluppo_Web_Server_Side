b2b92a873420c995b78eab23e57dbd0827420819
NicoMaker <nicola.marano02@gmail.com>
2025-03-28 17:35:02 +0100

    Refactor Stream.js to simplify event logging and enhance readability

--- Lezioni/2_Lezione/Progetti/3_Progetto/src/Stream.js
+++ Lezioni/2_Lezione/Progetti/3_Progetto/src/Stream.js
@@ -8,28 +8,31 @@ const reader = fs.createReadStream("Data/Social.json"),
   writer = fs.createWriteStream("Data/Social2.json"),
   writerGz = fs.createWriteStream("data/Social.json.gz"); // Crea uno stream di scrittura per il file JSON compresso
 
-// Quando lo stream 'reader' viene aperto, viene attivato l'evento "open".
-// La funzione di callback viene eseguita e stampa "Stream: open" nella console.
-reader.on("open", function (_chunk) {
-  console.log("Stream: open");
-});
+// // Quando lo stream 'reader' viene aperto, viene attivato l'evento "open".
+// // La funzione di callback viene eseguita e stampa "Stream: open" nella console.
+// reader.on("open", function (_chunk) {
+//   console.log("Stream: open");
+// });
 
-// Quando lo stream 'reader' viene chiuso, viene attivato l'evento "close".
-// La funzione di callback viene eseguita e stampa "Stream: close" nella console.
-reader.on("close", function (_chunk) {
-  console.log("Stream: close");
-});
+// // Quando lo stream 'reader' viene chiuso, viene attivato l'evento "close".
+// // La funzione di callback viene eseguita e stampa "Stream: close" nella console.
+// reader.on("close", function (_chunk) {
+//   console.log("Stream: close");
+// });
 
-// Si ascolta l'evento "data" emesso dal flusso `reader`
-// Ogni volta che il flusso riceve nuovi dati (chunk), questa funzione viene invocata
-reader.on("data", function (chunk) {
-  // I dati (chunk) vengono stampati sulla console
-  console.log(chunk);
-});
+// // Si ascolta l'evento "data" emesso dal flusso `reader`
+// // Ogni volta che il flusso riceve nuovi dati (chunk), questa funzione viene invocata
+// reader.on("data", function (chunk) {
+//   // I dati (chunk) vengono stampati sulla console
+//   console.log(chunk);
+// });
+
+reader.on("open", () => console.log("Stream: open"));
+reader.on("close", () => console.log("Stream: close"));
+reader.on("data", (chunk) => console.log(chunk));
 
 // Utilizza il metodo `pipe()` per trasferire i dati dallo stream 'reader' allo stream 'writer'.
 // Questo consente di leggere i dati in ingresso e scriverli direttamente nell'output.
 // reader.pipe(writer);
 
 reader.pipe(gz).pipe(writer); // I dati letti dallo stream 'reader' vengono compressi e poi scritti nello stream 'writerGz'.
-
