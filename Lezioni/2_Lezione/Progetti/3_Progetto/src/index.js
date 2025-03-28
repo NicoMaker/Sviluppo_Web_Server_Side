b3d197d25bf4fbd1f94f9d59b971ec8ef6bddf6d
NicoMaker <nicola.marano02@gmail.com>
2025-03-28 12:00:06 +0100

    impementato express e chiamate api

--- Lezioni/2_Lezione/Progetti/3_Progetto/src/index.js
+++ Lezioni/2_Lezione/Progetti/3_Progetto/src/index.js
@@ -3,12 +3,22 @@ const express = require("express"),
   host = `127.0.0.1`,
   port = 3000;
 
-server.get("/", (req, res) => {
+server.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
-server.get("/home", (req, res) => {
+server.get("/home", (_req, res) => {
   res.send("Benvenuto su Home!");
+});
+
+server.get("/json", (_req, res) => {
+  const rispostajson = {
+    nome: "Marco",
+    cognome: "Rossi",
+    eta: 30,
+  };
+
+  res.send(rispostajson);
 });
 
 server.listen(port, host, () =>
