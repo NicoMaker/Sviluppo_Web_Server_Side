07a0037debf1c1b7758491616114e1ce06bfb9c0
NicoMaker <nicola.marano02@gmail.com>
2025-03-28 12:31:14 +0100

    Add new API endpoints and JSON data files for CV project

--- Lezioni/2_Lezione/Progetti/3_Progetto/src/index.js
+++ Lezioni/2_Lezione/Progetti/3_Progetto/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express"),
+  fs = require("fs"),
+  path = require("path"),
   server = express(),
   host = `127.0.0.1`,
   port = 3000;
@@ -19,6 +21,29 @@ server.get("/json", (_req, res) => {
   };
 
   res.send(rispostajson);
+});
+
+server.get("/data", (_req, res) => {
+  const filePath = path.join(__dirname, "../Data/CV", "Competenze.json");
+
+  console.log(`Sto cercando il file in: ${filePath}`); // Aggiungi un log per vedere il percorso
+
+  // Leggi il file JSON
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Errore nella lettura del file JSON:", err);
+      res.status(500).send({ errore: "Errore nella lettura dei dati" });
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data); 
+      res.json(jsonData);
+    } catch (parseErr) {
+      console.error("Errore nel parsing del JSON:", parseErr);
+      res.status(500).send({ errore: "Errore nel formato dei dati" });
+    }
+  });
 });
 
 server.listen(port, host, () =>
