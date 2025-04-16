9aa6512385d9d7b143d6aa0fb3d5a26550785590
NicoMaker <nicola.marano02@gmail.com>
2025-04-16 10:09:01 +0200

    style: Standardize code formatting and improve consistency in index.js

--- Lezioni/6_Lezione/Progetti/1_Progetto/src/index.js
+++ Lezioni/6_Lezione/Progetti/1_Progetto/src/index.js
@@ -1,5 +1,5 @@
 // importo i moduli necessari
-const express = require('express');
+const express = require("express");
 
 // configuro la porta del server
 const port = 3000;
@@ -8,11 +8,27 @@ const port = 3000;
 const app = express();
 
 // definisco una rotta `/` alla quale rispondo con `Hello!`
-app.get('/', (_request, response) => {
-    response.send('Hello!')    
-})
+app.get("/", (_request, response) => {
+  response.send("Hello!");
+});
+
+app.post("/", (_request, response) => {
+  response.send("Hello! POST");
+});
+
+app.put("/", (_request, response) => {
+  response.send("Hello! PUT");
+});
+
+app.delete("/", (_request, response) => {
+  response.send("Hello! DELETE");
+});
+
+app.patch("/", (_request, response) => {
+  response.send("Hello! PATCH");
+});
 
 // avvio il server
 app.listen(port, () => {
-    console.log(`Server in ascolto su http://localhost:${port}`)
+  console.log(`Server in ascolto su http://localhost:${port}`);
 });
