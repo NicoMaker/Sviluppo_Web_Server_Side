1b7bb9956fdcddbe9b9e81164d0a5e0a79ae2340
NicoMaker <nicola.marano02@gmail.com>
2025-04-16 09:19:18 +0200

    Remove project files and dependencies for 1_Progetto, including package.json, HTML files, server and database scripts, and related assets.

--- Lezioni/6_Lezione/Progetti/1_Progetto/src/qrcode.js
+++
@@ -1,9 +0,0 @@
-// carico il modulo (la libreria) "qrcode"
-const qrcode = require("qrcode");
-
-// uso una funzione "toFile" presente dentro il modulo "qrcode" per creare un qrcode
-// creerà un file dentro "data/qrcode.png" rappresentante la stringa "Hello world"
-qrcode.toFile("data/qrcode.png", "Hello world", {
-  // imposto il livello di correzione di errore su H, 30% di danno sul qr code tollerabile
-  errorCorrectionLevel: "H",
-});
