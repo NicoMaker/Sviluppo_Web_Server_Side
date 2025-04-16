e12bf0c7d344b79918914adc51e86478a0160d30
NicoMaker <nicola.marano02@gmail.com>
2025-04-16 11:07:16 +0200

    feat: Add dynamic ID handling for GET, PUT, DELETE, and PATCH routes

--- Lezioni/6_Lezione/Progetti/1_Progetto/src/index.js
+++ Lezioni/6_Lezione/Progetti/1_Progetto/src/index.js
@@ -28,6 +28,31 @@ app.patch("/", (_request, response) => {
   response.send("Hello! PATCH");
 });
 
+// GET con ID dinamico
+app.get("/:id", (request, response) => {
+  const { id } = request.params; // Ottieni l'id dinamicamente
+  response.send(`Hello! GET ${id}!`);
+});
+
+
+// PUT con ID dinamico
+app.put("/:id", (request, response) => {
+  const { id } = request.params;
+  response.send(`Hello! PUT ${id}!`);
+});
+
+// DELETE con ID dinamico
+app.delete("/:id", (request, response) => {
+  const { id } = request.params;
+  response.send(`Hello! DELETE ${id}!`);
+});
+
+// PATCH con ID dinamico
+app.patch("/:id", (request, response) => {
+  const { id } = request.params;
+  response.send(`Hello! PATCH ${id}!`);
+});
+
 // avvio il server
 app.listen(port, () => {
   console.log(`Server in ascolto su http://localhost:${port}`);
